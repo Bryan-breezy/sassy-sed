1bcd410f2f6b22b538892eeea59bc7bb4c661240
Bryan-breezy <83723012+Bryan-breezy@users.noreply.github.com>
2025-11-21 16:46:31 +0300

    Update ProductHero.tsx

--- components/ProductHero.tsx
+++ components/ProductHero.tsx
@@ -112,7 +112,7 @@ export const ProductHero: React.FC<ProductHeroProps> = ({
 
             {/* Description */}
             {product.description && (
-              <p className="text-base sm:text-lg md:text-xl lg:text-2xl leading-relaxed text-neutral-900 max-w-3xl">
+              <p className="text-base sm:text-lg md:text-xl lg:text-2xl leading-relaxed text-stone-400 max-w-3xl">
                 {product.description}
               </p>
             )}
